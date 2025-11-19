701d495700dc0cbbfa546916b633e370171293f1
Abdelwaheb <bouchahwaabdelwaheb@fsg.u-gabes.tn>
2025-11-19 15:53:18 +0100

    ajout 'plan' att

--- lib/models/entrepreneur_model.dart
+++ lib/models/entrepreneur_model.dart
@@ -1,45 +1,50 @@
-class Student {
+class Entrepreneur {
   final String name;
   final String cin;
   final String nomPole;
-  final int finTotale;
+  final String plan;
+  final int finTotal;
   final int score;
 
-  factory Student.emptyStudent() {
-    return Student(
+  factory Entrepreneur.emptyUser() {
+    return Entrepreneur(
       name: "Not Available !",
       cin: "Not Available !",
       nomPole: "Not Available !",
-      finTotale: 0,
+      plan: "Free",
+      finTotal: 0,
       score: 0,
     );
   }
 
-  Student({
+  Entrepreneur({
     required this.name,
     required this.cin,
     required this.nomPole,
-    required this.finTotale,
+    required this.plan,
+    required this.finTotal,
     required this.score,
   });
 
-  factory Student.fromFireStore(Map<String, dynamic> data) {
-    return Student(
+  factory Entrepreneur.fromFireStore(Map<String, dynamic> data) {
+    return Entrepreneur(
       name: data['Nom'] ?? "Not Available !",
       cin: data['Cin'] ?? "Not Available",
       nomPole: data['Nompole'] ?? "Not Available",
-      finTotale: data['Fintot'] ?? "Not Available",
+      plan: data['Plan'] ?? "Not Available",
+      finTotal: data['Fintot'] ?? "Not Available",
       score: data['Score'] ?? "Not Available",
     );
   }
 
   Map<String, dynamic> toFirestore() {
     return {
-      'nom': name,
-      'cin': cin,
-      'nom_pole': nomPole,
-      'fin_tot': finTotale,
-      'score': score,
+      'Nom': name,
+      'Cin': cin,
+      'Nompole': nomPole,
+      'Plan': plan,
+      'Fintot': finTotal,
+      'Score': score,
     };
   }
 }
